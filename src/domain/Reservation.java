412f6aa220f62cfede3fe08baf20f14960fb7a91
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-10-27 19:59:23 -0500

    Añadiendo clase Reservation

---
+++ src/domain/Reservation.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.io.Serializable;
+
+public class Reservation implements Serializable{
+     private Guest guest;
+    //private Room room;
+    private int resNumber = 0;
+    private boolean status;
+
+    public Reservation(Guest guest /*,Room room */){
+        this.guest = guest;
+        //this.room = room;
+        this.resNumber += 1;
+        this.status = true;
+    }
+
+    public Guest getGuest(){
+        return guest;
+    }
+
+    /*public Room getRoom(){
+        return room;
+    } */
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public int getReservationNumber(){
+        return resNumber;
+    }
+
+    public void setGuest(Guest guest){
+        this.guest = guest;
+    }
+
+    public void disableReservation(){
+        this.status = false;
+    }
+
+    /*public void setRoom(Room room){
+        this.room = room;    
+    } */
+}
