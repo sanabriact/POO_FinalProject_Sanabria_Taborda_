0c99eef1bf05b767ce2983e3ccb3de2967d90900
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-10-28 13:53:32 -0500

    Añadiendo data

--- src/data/HotelStorage.java
+++ src/data/HotelStorage.java
@@ -1,4 +1,25 @@
 package data;
-public class HotelStorage {
 
+import java.io.*;
+import domain.*;
+
+public class HotelStorage {
+    
+    public static void save(Hotel hotel, String filename){
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))){
+            out.writeObject(hotel);
+            System.out.println("Hotel data saved succesfully.");
+        }catch(IOException exception){
+            System.err.println("an error occurred while saving hotel data.");
+        }
+    }
+
+    public static Hotel load(String filename){
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))){
+            return (Hotel) in.readObject();
+        }catch(IOException | ClassNotFoundException exception){
+            System.err.println("an error occurred while loading hotel data.");
+            return null;
+        }
+    }
 }
