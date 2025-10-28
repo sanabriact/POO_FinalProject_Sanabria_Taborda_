1a629d24e40db5b7d50387ec294a85334d0b72e1
Juan Felipe Sanabria <juan.sanabria64903@ucaldas.edu.co>
2025-10-28 10:20:01 -0500

    Añadiendo clase

--- src/domain/Room.java
+++ src/domain/Room.java
@@ -3,5 +3,67 @@ package domain;
 import java.io.Serializable;
 
 public class Room implements Serializable{
+    private int roomNum, beds, baths;
+    private String roomType = "";
+
+    public Room(int roomNum,int beds, int baths,String roomType) {
+        this.roomNum = roomNum; 
+        this.beds = beds;
+        this.baths = baths;
+        this.roomType = roomType;
     
+    }
+
+    public int getRoomNum() {
+        return this.roomNum;
+    }
+
+    public int getRoomBeds() {
+        return this.beds;
+    }
+
+    public int getRoomBaths() {
+        return this.baths;
+    }
+
+    public String getRoomType() {
+        return this.roomType;
+    }
+
+    public boolean getSeaView() {
+        if(this.roomType.equals("Basic")) {
+            return false;
+        }
+         
+        return true;
+    }
+
+    public double getPrice() {
+        if(this.roomType.equals("Basic")) {
+            return 39.99;
+        } 
+        
+        if(this.roomType.equals("Medium")) {
+            return 59.99;
+        }
+
+        return 99.99;
+    }
+
+    public void setRoomNum(int roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public void setRoomBeds(int beds) {
+        this.beds = beds;;
+    }
+
+    public void setRoomBaths(int baths) {
+        this.baths = baths;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
 }
