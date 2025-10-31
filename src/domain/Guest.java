d92909d85f34f3c7d4c426fffba31aa00880e49a
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-10-31 15:23:24 -0500

    toString()

--- src/domain/Guest.java
+++ src/domain/Guest.java
@@ -2,64 +2,69 @@ package domain;
 
 import java.io.Serializable;
 
-public class Guest implements Serializable{
+public class Guest implements Serializable {
     private String name;
     private String email;
     private int phoneNumber;
     private int id;
 
-    public Guest(String name, String email, int phoneNumber, int id){
+    public Guest(String name, String email, int phoneNumber, int id) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.id = id;
     }
 
-    public String getGuestName(){
+    public String getGuestName() {
         return name;
     }
 
-    public String getGuestEmail(){
+    public String getGuestEmail() {
         return email;
     }
 
-    public int getGuestPhoneNum(){
+    public int getGuestPhoneNum() {
         return phoneNumber;
     }
 
-    public int getGuestId(){
+    public int getGuestId() {
         return id;
     }
 
-    public void setGuestName(String name){
-        if(name.isEmpty()){
+    public void setGuestName(String name) {
+        if (name.isEmpty()) {
             this.name = null;
-        }else{
+        } else {
             this.name = name;
         }
     }
 
-    public void setGuestEmail(String email){
-        if(email.isEmpty()){
+    public void setGuestEmail(String email) {
+        if (email.isEmpty()) {
             this.email = null;
-        }else{
+        } else {
             this.email = email;
         }
     }
 
-    public void setPhoneNumber(int phoneNumber){
-        if(phoneNumber > 0){
+    public void setPhoneNumber(int phoneNumber) {
+        if (phoneNumber > 0) {
             this.phoneNumber = phoneNumber;
-        }else{
+        } else {
             this.phoneNumber = 0;
         }
     }
 
-    public void setGuestId(int id){
-        if(id > 0){
+    public void setGuestId(int id) {
+        if (id > 0) {
             this.id = id;
-        }else{
+        } else {
             this.id = 0;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{id=" + id + ", name=" + name + ", email=" + email + ", phoneNumber=" + phoneNumber + "}";
     }
 }
