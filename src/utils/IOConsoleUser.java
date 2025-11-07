d9b3f1dbb0ae15cfe8b3f0af7058db8fb287381a
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-07 14:07:41 -0500

    Añadiendo utils

---
+++ src/utils/IOConsoleUser.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.util.Scanner;
+
+public class IOConsoleUser implements IOUser{
+    private static Scanner input = new Scanner(System.in);
+
+    @Override
+    public String inputText(String message){
+        System.out.println(message);
+        return input.nextLine();
+    }
+
+    @Override
+    public void writeLine(String message){
+        System.out.println(message);
+    }
+
+    @Override
+    public int inputInt(String message){
+        return Integer.parseInt(inputText(message));
+    }
+
+    @Override
+    public float inputFloat(String message){
+        return Float.parseFloat(inputText(message));
+    }
+
+    @Override
+    public double inputDouble(String message){
+        return Double.parseDouble(inputText(message));
+    }
+
+
+
+
+}
---
+++ src/utils/IOUser.java
@@ -0,0 +1,10 @@
+package utils;
+
+public interface IOUser {
+    public abstract String inputText(String title);
+    public abstract void writeLine(String message);
+    public abstract int inputInt(String title);
+    public abstract float inputFloat(String title);
+    public abstract double inputDouble(String title);
+
+}
