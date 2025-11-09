8e0338e5f813399ca25225ade1b919e78f9d6271
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-09 11:32:59 -0500

    Cambios Main.java

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -14,14 +14,42 @@ public class Main {
         int option;
 
         while (menu) {
+            // HOTEL MENU
             showHotelMenu();
-            option = keyboard.inputInt("- Enter an option:");
+            option = keyboard.inputInt("- Enter an option: ");
             switch(option){
                 case 1 -> {
 
                 }
                 case 2 ->{
+                    //Administrator menu
+                    showAdminMenu();
+                    option = keyboard.inputInt("- Enter an option: ");
+                    switch(option){
+                        case 1 -> {
+                            //Reservations
+                            showReservationMenu();
+                            option = keyboard.inputInt(" - Enter an option: ");
+                            switch (option) {
+                                case 1 -> {
+                                    //Add reservation
 
+                                }
+                                case 2 -> {
+
+                                }
+                            
+                                default ->{
+
+                                }
+                            }
+                        }
+                        case 2 -> {
+                            //Reservation record
+                        }
+                        //case 3 ->{//Employees}
+                        default ->{}
+                    }
                 }
                 default ->{
                     menu = false;
@@ -36,4 +64,23 @@ public class Main {
         System.out.println("(2) Administrator menu.");
         System.out.println("(3) Save and exit.");
     }
+
+    private static void showAdminMenu(){
+        System.out.println("- - - - - - - - ADMINISTRATOR MENU - - - - - - - - ");
+        System.out.println("(1) Reservations.");
+        System.out.println("(2) Reservation record.");
+        System.out.println("(3) Go back.");
+        //System.out.println("(4) Employees.");
+
+    }
+
+    private static void showReservationMenu(){
+        System.out.println("- - - - - - - - RESERVATION MENU - - - - - - - - ");
+        System.out.println("(1) Add reservation.");
+        System.out.println("(2) Show active Reservations.");
+        System.out.println("(3) Change reservation data.");
+        System.out.println("(4) Go back.");
+    }
+
+    
 }
