55b09e0b1e98180aa19b7c69451e4c6a20ca3025
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-21 18:38:47 -0500

    New changes and attributes for Employee.java

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -1,26 +1,62 @@
 package domain;
 
 public class Employee extends Guest {
+    // static int because all Employees share this attribute For each new reservation "employeeNumber" increases by one
+    private static int nextNumber = 0;
     private double salary;
+    private int employeeNumber;
+    private String position;
+    private boolean active;
 
-    public Employee(String name, String email, long phoneNumber, long id, double salary){
+    public Employee(String name, String email, long phoneNumber, long id, String position, double salary) {
         super(name, email, phoneNumber, id);
         this.salary = salary;
+        this.position = position;
+        this.employeeNumber = nextNumber++;
+        this.active = true;
     }
 
-    public double getSalary(){
+    public String getPosition() {
+        return position;
+    }
+
+    public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary){
-        if(salary > 0){
+    public boolean getActive() {
+        return active;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setSalary(double salary) {
+        if (salary > 0) {
             this.salary = salary;
-        }else{
+        } else {
             this.salary = 0;
         }
     }
 
-        
+    public void setPosition(String position) {
+        if (!position.isEmpty()) {
+            this.position = position;
+        } else {
+            this.position = null;
+        }
+    }
 
+    public void dismissEmployee() {
+        this.active = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{id=" + getId() + ", name=" + getName() + ", email=" + getEmail() + ", phoneNumber="
+                + getPhoneNum() + ", position=" + position + ", salary=$" + salary + ", active=" + active
+                + ", employeeNumber=" + employeeNumber + "}";
+    }
 
 }
