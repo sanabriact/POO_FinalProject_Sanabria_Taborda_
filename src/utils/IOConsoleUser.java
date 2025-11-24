7e05355f3cd79e42d9979e827b6a35fe592e19b1
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-24 13:55:48 -0500

    Adding try catch

--- src/utils/IOConsoleUser.java
+++ src/utils/IOConsoleUser.java
@@ -18,22 +18,47 @@ public class IOConsoleUser implements IOUser{
 
     @Override
     public int inputInt(String message){
-        return Integer.parseInt(inputText(message));
+        while (true) {
+            try {
+                return Integer.parseInt(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer number.");
+            }
+        }
+
     }
 
     @Override
     public long inputLong(String message){
-        return Long.parseLong(inputText(message));
+        while (true) {
+            try {
+                return Long.parseLong(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer number.");
+            }
+        }
     }
 
     @Override
     public float inputFloat(String message){
-        return Float.parseFloat(inputText(message));
+        while (true) {
+            try {
+                return Float.parseFloat(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer or decimal number.");
+            }
+        }
     }
 
     @Override
     public double inputDouble(String message){
-        return Double.parseDouble(inputText(message));
+        while (true) {
+            try {
+                return Double.parseDouble(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n- You must enter an integer or decimal number.");
+            }
+        }
     }
 
 
