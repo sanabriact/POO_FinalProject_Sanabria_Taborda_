7810c620f90fcabc84b6f2ca836ae79227232862
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-25 00:23:44 -0500

    Adding try/catch

--- src/utils/IODialogUser.java
+++ src/utils/IODialogUser.java
@@ -2,30 +2,56 @@ package utils;
 
 import javax.swing.JOptionPane;
 
-public class IODialogUser implements IOUser{
+public class IODialogUser implements IOUser {
     @Override
-    public String inputText(String message){
-        return JOptionPane.showInputDialog(null, message);
+    public String inputText(String message) {
+        {
+            return JOptionPane.showInputDialog(null, message);
+        }
     }
 
     @Override
-    public void writeLine(String message){
+    public void writeLine(String message) {
         JOptionPane.showMessageDialog(null, message);
     }
 
-    public int inputInt(String message){
-        return Integer.parseInt(inputText(message));
+    public int inputInt(String message) {
+        while (true) {
+            try {
+                return Integer.parseInt(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer number.");
+            }
+        }
     }
 
-    public long inputLong(String message){
-        return Long.parseLong(inputText(message));
+    public long inputLong(String message) {
+        while (true) {
+            try {
+                return Long.parseLong(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer number.");
+            }
+        }
     }
 
-    public float inputFloat(String message){
-        return Float.parseFloat(inputText(message));
+    public float inputFloat(String message) {
+        while (true) {
+            try {
+                return Float.parseFloat(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer or decimal number.");
+            }
+        }
     }
 
-    public double inputDouble(String message){
-        return Double.parseDouble(inputText(message));
+    public double inputDouble(String message) {
+        while (true) {
+            try {
+                return Double.parseDouble(inputText(message));
+            } catch (Exception e) {
+                writeLine("\n -You must enter an integer or decimal number.");
+            }
+        }
     }
 }
