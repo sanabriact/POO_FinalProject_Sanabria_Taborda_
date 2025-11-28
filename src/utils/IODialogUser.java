47b1cd3d4faf8339dced6cd045f5a38d2248ccf5
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-11-28 11:49:20 -0500

    Doing commits on IODialogUser.java methods

--- src/utils/IODialogUser.java
+++ src/utils/IODialogUser.java
@@ -3,19 +3,19 @@ package utils;
 import javax.swing.JOptionPane;
 
 public class IODialogUser implements IOUser {
+
     @Override
-    public String inputText(String message) {
-        {
-            return JOptionPane.showInputDialog(null, message);
-        }
+    public String inputText(String message) { // gets text input through dialog
+        return JOptionPane.showInputDialog(null, message);
     }
 
     @Override
-    public void writeLine(String message) {
+    public void writeLine(String message) { // shows a message dialog
         JOptionPane.showMessageDialog(null, message);
     }
 
-    public int inputInt(String message) {
+    @Override
+    public int inputInt(String message) { // reads an int with validation using dialogs
         while (true) {
             try {
                 return Integer.parseInt(inputText(message));
@@ -25,7 +25,8 @@ public class IODialogUser implements IOUser {
         }
     }
 
-    public long inputLong(String message) {
+    @Override
+    public long inputLong(String message) { // reads a long with validation using dialogs
         while (true) {
             try {
                 return Long.parseLong(inputText(message));
@@ -35,7 +36,8 @@ public class IODialogUser implements IOUser {
         }
     }
 
-    public float inputFloat(String message) {
+    @Override
+    public float inputFloat(String message) { // reads a float with validation using dialogs
         while (true) {
             try {
                 return Float.parseFloat(inputText(message));
@@ -45,7 +47,8 @@ public class IODialogUser implements IOUser {
         }
     }
 
-    public double inputDouble(String message) {
+    @Override
+    public double inputDouble(String message) { // reads a double with validation using dialogs
         while (true) {
             try {
                 return Double.parseDouble(inputText(message));
